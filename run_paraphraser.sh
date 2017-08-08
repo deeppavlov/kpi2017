d129478a47c1ee26610ac575039d0e3c5d17239b
yoptar <yoptar@gmail.com>
2017-08-08 16:00:56 +0300

    Added batch size parameter

--- run_paraphraser.sh
+++ run_paraphraser.sh
@@ -2,5 +2,6 @@
 python3 ./train_model.py -t parlai_tasks.paraphrases.agents \
                          -m parlai_agents.paraphraser.paraphraser:ParaphraserAgent \
                          -mf /tmp/paraphraser \
+                         --batchsize 16 \
                          --display-examples True \
                          --max-train-time 10
