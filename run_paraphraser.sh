1a5121c226ba3aefeedd440cf80da72f547f501d
leonid <puleon@mail.ru>
2017-08-17 19:45:24 +0300

    paraphraser save method added.

--- run_paraphraser.sh
+++ run_paraphraser.sh
@@ -8,9 +8,9 @@ python3 ./train_model.py -t parlai_tasks.paraphrases.agents \
                          --num-epochs 10 \
                          --log-every-n-secs -1 \
                          --log-every-n-epochs 1 \
-                         --learning_rate 0.00001 \
+                         --learning_rate 0.0001 \
                          --hidden_dim 200 \
                          --embedding_file 'yalen_sg_word_vectors_300.txt' \
-                         --validation-every-n-epochs 1
+                         --validation-every-n-epochs 5
 #                         --pretrained_model '/tmp/my_model/my_model'
 #                         --validation-patience 5 \
