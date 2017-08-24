c203d3ba7efad293988128d65ef7b5aa0a02c205
yoptar <yoptar@gmail.com>
2017-08-24 18:28:34 +0300

    Lowered tensorflow's memory requirement for paraphraser task

--- parlai_agents/paraphraser/model.py
+++ parlai_agents/paraphraser/model.py
@@ -5,7 +5,7 @@ import json
 import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
 config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.95
+config.gpu_options.per_process_gpu_memory_fraction = 0.8
 config.gpu_options.visible_device_list = '0'
 set_session(tf.Session(config=config))
 
