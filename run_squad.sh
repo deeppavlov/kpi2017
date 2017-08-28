7569b6b402f6a7f69354332fb906db48843e3556
yoptar <yoptar@gmail.com>
2017-08-28 18:00:50 +0300

    Set basic metric for squad task to be f1

--- run_squad.sh
+++ run_squad.sh
@@ -2,11 +2,12 @@
 python3 utils/train_model.py -t squad \
                          -m deeppavlov.agents.squad.squad:SquadAgent \
                          -mf /tmp/squad_model \
-                         --batchsize 10 \
+                         --batchsize 64 \
                          --display-examples False \
                          --max-train-time -1 \
                          --num-epochs -1 \
-                         --log-every-n-secs 60 \
+                         --log-every-n-secs 20 \
                          --log-every-n-epochs -1 \
                          --validation-every-n-secs 600 \
+                         --chosen-metric f1 \
                          --embedding_file '/home/anatoly/data/fasttext/wiki-news-300d-1M.vec'
