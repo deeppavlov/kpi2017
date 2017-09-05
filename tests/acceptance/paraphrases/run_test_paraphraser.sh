5912fe6dea73c957258ad20a67ba924e413593e1
leonid <puleon@mail.ru>
2017-09-05 16:06:05 +0300

    changed tmp folder to build.

--- tests/acceptance/paraphrases/run_test_paraphraser.sh
+++ tests/acceptance/paraphrases/run_test_paraphraser.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
-
-./run_paraphraser.sh | tail -n 1 | { read str; value=${str:11:20}; echo $value'>'0.8; } | bc -l
+python3 ./utils/train_model.py -t deeppavlov.tasks.paraphrases.agents \
+                         -m deeppavlov.agents.paraphraser.paraphraser:EnsembleParaphraserAgent \
+                         -mf /build/paraphraser_maxpool_match \
+                         --model_files /build/maxpool_match \
+                         --datatype test \
+                         --batchsize 256 \
+                         --display-examples False \
+                         --fasttext_embeddings_dict "/build/paraphraser.emb" \
+                         --fasttext_model '/build/ft_0.8.3_nltk_yalen_sg_300.bin' \
+                         --cross-validation-splits-count 5 \
+                         --chosen-metric f1 | tail -n 1 | { read str; value=${str:11:20}; echo $value'>'0.8; } | bc -l
