abf429928c1a9c3a14fbeaf16aaacad3e1b95b3e
dilyararimovna <dilyara.rimovna@gmail.com>
2017-09-06 14:20:06 +0300

    excess print deleted

--- deeppavlov/agents/insults/embeddings_dict.py
+++ deeppavlov/agents/insults/embeddings_dict.py
@@ -43,8 +43,7 @@ class EmbeddingsDict(object):
             except:
                 raise RuntimeError('Looks like the `IPAVLOV_FTP` variable is set incorrectly')
         self.fasttext_model = fasttext.load_model(self.fasttext_model_file)
-        print('fasttext model loaded, you:', self.fasttext_model['you'])
-
+        
     def add_items(self, sentence_li):
         for sen in sentence_li:
             tokens = sen.split(' ')
