1) Fixed problems with masking 2) Added 15 token window for answer 3) Added answer selection by maximizing start*end prob

deeppavlov/agents/squad/layers.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import keras.backend as K
-from keras.layers import TimeDistributed, Lambda, Dense, Activation, multiply, LSTM, Bidirectional, Dropout
+from keras.layers import TimeDistributed, Lambda, Dense, Activation, multiply, LSTM, Bidirectional, Dropout, merge
 from keras.activations import softmax as Softmax
 
 '''
@@ -21,17 +21,26 @@ def concatenate(values):
 def flatten(value):
     return tf.contrib.layers.flatten(value)
 
+def masked_softmax(tensor, mask, expand=2, axis=1):
+    '''Masked soft-max using Lambda and merge-multiplication'''
+    mask = tf.expand_dims(mask, axis=expand)
+    exponentiate = Lambda(lambda x: K.exp(x))(tensor)
+    masked = tf.multiply(exponentiate, mask)
+    div = tf.expand_dims(tf.reduce_sum(masked, axis=axis), axis=axis)
+    predicted = tf.divide(masked, div)
+    return predicted
+
 '''
 ---------- Layers -------------------------
 '''
 
-def learnable_wiq(context, question, layer_dim):
+def learnable_wiq(context, question, question_mask, layer_dim):
     ''' Aligned question embedding'''
     question_enc = TimeDistributed(Dense(units=layer_dim, activation='relu'))(question)
     context_enc = TimeDistributed(Dense(units=layer_dim, activation='relu'))(context)
     question_enc = Lambda(lambda q: tf.transpose(q, [0, 2, 1]))(question_enc)
     matrix = Lambda(lambda q: tf.matmul(q[0], q[1]))([context_enc, question_enc])
-    coefs = Lambda(lambda q: Softmax(q, axis=2))(matrix)
+    coefs = Lambda(lambda q: masked_softmax(matrix, question_mask, axis=2, expand=1))([matrix, question_mask])
     aligned_question_enc = Lambda(lambda q: tf.matmul(q[0], q[1]))([coefs, question])
     return(aligned_question_enc)
 
@@ -76,19 +85,19 @@ def projection(encoding, W, dropout_rate):
     return proj
 
 
-def question_attn_vector(question_encoding, context_encoding):
+def question_attn_vector(question_encoding, question_mask, context_encoding):
     ''' Attention over question '''
     question_attention_vector = TimeDistributed(Dense(1))(question_encoding)
-    question_attention_vector = Lambda(lambda q: Softmax(q, axis=1))(question_attention_vector)
+    # apply masking
+    question_attention_vector = Lambda(lambda q: masked_softmax(q[0], q[1]))([question_attention_vector, question_mask])
     # apply the attention
     question_attention_vector = Lambda(lambda q: q[0] * q[1])([question_encoding, question_attention_vector])
     question_attention_vector = Lambda(lambda q: K.sum(q, axis=1))(question_attention_vector)
     question_attention_vector = Lambda(lambda q: repeat_vector(q[0], q[1]))([question_attention_vector, context_encoding])
-    question_attention_vector = Lambda(lambda q: Softmax(q, axis=1))(question_attention_vector)
     return question_attention_vector
 
 
-def answer_start_pred(context_encoding, question_attention_vector, W, dropout_rate):
+def answer_start_pred(context_encoding, question_attention_vector, context_mask, W, dropout_rate):
     ''' Answer start prediction layer '''
     answer_start = Lambda(lambda arg:
                           concatenate([arg[0], arg[1], arg[2]]))([
@@ -99,12 +108,14 @@ def answer_start_pred(context_encoding, question_attention_vector, W, dropout_ra
     answer_start = TimeDistributed(Dense(W, activation='relu'))(answer_start)
     answer_start = Dropout(rate=dropout_rate)(answer_start)
     answer_start = TimeDistributed(Dense(1))(answer_start)
+
+    # apply masking
+    answer_start = Lambda(lambda q: masked_softmax(q[0], q[1]))([answer_start, context_mask])
     answer_start = Lambda(lambda q: flatten(q))(answer_start)
-    answer_start = Activation('softmax')(answer_start)
     return answer_start
 
 
-def answer_end_pred(context_encoding, question_attention_vector, answer_start_distribution, W, dropout_rate):
+def answer_end_pred(context_encoding, question_attention_vector, context_mask, answer_start_distribution, W, dropout_rate):
     ''' Answer end prediction layer '''
     # Answer end prediction depends on the start prediction
     def s_answer_feature(x):
@@ -132,6 +143,8 @@ def answer_end_pred(context_encoding, question_attention_vector, answer_start_di
     answer_end = TimeDistributed(Dense(W, activation='relu'))(answer_end)
     answer_end = Dropout(rate=dropout_rate)(answer_end)
     answer_end = TimeDistributed(Dense(1))(answer_end)
+
+    # apply masking
+    answer_end = Lambda(lambda q: masked_softmax(q[0], q[1]))([answer_end, context_mask])
     answer_end = Lambda(lambda q: flatten(q))(answer_end)
-    answer_end = Activation('softmax')(answer_end)
     return answer_end

deeppavlov/agents/squad/model.py
@@ -88,7 +88,7 @@ class SquadModel(object):
         answ_start = []
         answ_end = []
 
-        x, y = [batch[0], batch[1], batch[3]], [cat(batch[5]), cat(batch[6])]
+        x, y = [batch[0], batch[1], batch[3], batch[2], batch[4]], [cat(batch[5]), cat(batch[6])]
 
         output = self.model.train_on_batch(x, y)
         self.train_loss.update(output[0])
@@ -110,7 +110,7 @@ class SquadModel(object):
 
     def predict(self, batch):
 
-        score_s, score_e = self.model.predict_on_batch([batch[0], batch[1], batch[3]])
+        score_s, score_e = self.model.predict_on_batch([batch[0], batch[1], batch[3], batch[2], batch[4]])
 
         text = batch[-2]
         spans = batch[-1]
@@ -152,11 +152,15 @@ class SquadModel(object):
         Q = Input(shape=(None, self.word_embedding_dim), name='question_input')
         P_f = Input(shape=(None, self.context_embedding_dim - self.word_embedding_dim), name='context_features')
 
+        '''Masking inputs'''
+        P_mask = Input(shape=(None,), name='context_mask')
+        Q_mask = Input(shape=(None,), name='question_mask')
+
         passage_input = Lambda(lambda q: tf.concat(q, axis=2))([P, P_f])
         question_input = Q
 
         ''' Aligned question embedding '''
-        aligned_question = learnable_wiq(P, Q, layer_dim=self.aligned_question_dim)
+        aligned_question = learnable_wiq(P, Q, Q_mask, layer_dim=self.aligned_question_dim)
         passage_input = Lambda(lambda q: tf.concat(q, axis=2))([P, P_f, aligned_question])
 
         ''' Emdedding dropout (with similar mask for all timesteps) '''
@@ -190,15 +194,15 @@ class SquadModel(object):
         question_encoding = projection(question_encoding, self.projection_dim, self.linear_dropout)
 
         '''Attention over question'''
-        question_attention_vector = question_attn_vector(question_encoding, passage_encoding)
+        question_attention_vector = question_attn_vector(question_encoding, Q_mask, passage_encoding)
 
         '''Answer span prediction'''
         # Answer start prediction
-        answer_start = answer_start_pred(passage_encoding, question_attention_vector, self.pointer_dim, self.linear_dropout)
+        answer_start = answer_start_pred(passage_encoding, question_attention_vector, P_mask, self.pointer_dim, self.linear_dropout)
         # Answer end prediction
-        answer_end = answer_end_pred(passage_encoding, question_attention_vector, answer_start, self.pointer_dim, self.linear_dropout)
+        answer_end = answer_end_pred(passage_encoding, question_attention_vector, P_mask, answer_start, self.pointer_dim, self.linear_dropout)
 
-        input_placeholders = [P, P_f, Q]
+        input_placeholders = [P, P_f, Q, P_mask, Q_mask]
         inputs = input_placeholders
         outputs = [answer_start, answer_end]
 

deeppavlov/agents/squad/utils.py
@@ -159,20 +159,20 @@ def batchify(batch, null=0):
     max_length = max([d.shape[0] for d in docs])
     emb_dim = docs[0].shape[1]
     x1 = np.zeros((len(docs), max_length, emb_dim))
-    x1_mask = np.ones((len(docs), max_length, emb_dim))
+    x1_mask = np.zeros((len(docs), max_length))
     x1_f = np.zeros((len(docs), max_length, features[0].shape[1]))
     for i, d in enumerate(docs):
         x1[i, :d.shape[0], :] = d
-        x1_mask[i, :d.shape[0], :] = 0
+        x1_mask[i, :d.shape[0]] = 1.0
         x1_f[i, :d.shape[0]] = features[i]
 
     # Batch questions
     max_length = max([q.shape[0] for q in questions])
     x2 = np.zeros((len(questions), max_length, emb_dim))
-    x2_mask = np.ones((len(questions), max_length, emb_dim))
+    x2_mask = np.zeros((len(questions), max_length))
     for i, q in enumerate(questions):
         x2[i, :q.shape[0], :] = q
-        x2_mask[i, :q.shape[0]] = 0
+        x2_mask[i, :q.shape[0]] = 1.0
 
     # Maybe return without targets
     if len(batch[0]) == NUM_INPUTS + NUM_EXTRA: