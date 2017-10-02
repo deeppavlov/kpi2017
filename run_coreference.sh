bf52627084e1a94c2ce690a1c6a856732a393706
Максим Петров <petrov@dgx2.ipavlov.mipt.ru>
2017-10-02 18:00:07 +0300

    v0.5.2

--- run_coreference.sh
+++ run_coreference.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
 #parlai.agents.repeat_label.repeat_label
-python3 ./utils/train_model.py -t deeppavlov.tasks.coreference.agents:BaseTeacher \
-                         -m deeppavlov.agents.coreference.RepeatLabelAgent:RepeatLabelAgent \
+export CUDA_VISIBLE_DEVICES=0; python3 ./utils/train_model.py -t deeppavlov.tasks.coreference.agents:BaseTeacher \
+                         -m deeppavlov.agents.coreference.agents:CoreferenceAgent \
                          -mf ./build/coreference \
                          --cor coreference \
                          --data-path ./build \
 		         --language russian \
+			 --name main \
+			 --pretrained_model False \
                          --split 0.2 \
                          --random-seed None \
                          -dt train:ordered \
                          --batchsize 1 \
                          --display-examples True \
                          --max-train-time -1 \
-                         --validation-every-n-epochs 1 \
-                         --log-every-n-epochs 1 \
+                         --validation-every-n-epochs 10 \
+                         --log-every-n-epochs 5 \
                          --log-every-n-secs -1 \
-                         --chosen-metric f1
-  
+                         --chosen-metric conll-F-1 \
+
