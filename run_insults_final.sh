2e83ff2d96e6898e8adfd92f8a2f92cbb9fe05c5
dilyararimovna <dilyara.rimovna@gmail.com>
2017-10-05 15:40:22 +0300

    upd: Change validation patience in final sh

--- run_insults_final.sh
+++ run_insults_final.sh
@@ -27,7 +27,7 @@ python utils/train_model.py -t deeppavlov.tasks.insults.agents \
                             --fasttext_embeddings_dict ./build/emb_dict.emb \
                             --cross-validation-splits-count 3 \
                             -ve 10 \
-                            -vp 1 \
+                            -vp 5 \
                             --chosen-metric auc
 
 python3 ./utils/train_model.py -t deeppavlov.tasks.insults.agents:FullTeacher \
