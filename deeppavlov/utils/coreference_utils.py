abb318b31749b295a2a6cafd80027914b311d2a0
yurakuratov <yurakuratov@gmail.com>
2017-10-30 13:59:02 +0300

    docs: add docstrings

--- deeppavlov/utils/coreference_utils.py
+++ deeppavlov/utils/coreference_utils.py
@@ -1,3 +1,16 @@
+"""
+Copyright 2017 Neural Networks and Deep Learning lab, MIPT
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import json
 import numpy as np
 import os
@@ -7,6 +20,17 @@ from collections import defaultdict
 
 
 def score(scorer, keys_path, predicts_path):
+    """Scores predicted files
+
+    Args:
+        scorer: path to official CoNLL-2012 perl scorer
+        keys_path: path to ground truth conll files
+        predicts_path: path to predicted conll files
+
+    Returns:
+        dict with scores
+    """
+
     key_files = list(filter(lambda x: x.endswith('conll'), os.listdir(keys_path)))
     pred_files = list(filter(lambda x: x.endswith('conll'), os.listdir(predicts_path)))
 
@@ -64,7 +88,7 @@ def score(scorer, keys_path, predicts_path):
         results[metric] = {'p': p, 'r': r, 'f-1': f}
 
     # muc bcub ceafe
-    conllf1 = np.mean(f1[:2] + f1[-1:])  # wtf
+    conllf1 = np.mean([f1[0], f[1], f1[-1]])
     res['using'] = 'using {}/{}'.format(k, 4 * len(key_files))
     res['avg-F-1'] = np.mean(f1)
     res['conll-F-1'] = conllf1
