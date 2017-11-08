c14ebc91e3061be09030d7bfd981902286fa3f03
leonid <puleon@mail.ru>
2017-11-08 12:30:39 +0300

    docs: Add docstrings to paraphraser

--- deeppavlov/agents/paraphraser/metrics.py
+++ deeppavlov/agents/paraphraser/metrics.py
@@ -19,10 +19,12 @@ from keras import backend as K
 
 def precision(y_true, y_pred):
     """Precision metric.
+
     Only computes a batch-wise average of precision.
     Computes the precision, a metric for multi-label classification of
     how many selected items are relevant.
     """
+
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
     precision = true_positives / (predicted_positives + K.epsilon())
@@ -31,10 +33,12 @@ def precision(y_true, y_pred):
 
 def recall(y_true, y_pred):
     """Recall metric.
+
     Only computes a batch-wise average of recall.
     Computes the recall, a metric for multi-label classification of
     how many relevant items are selected.
     """
+
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
     recall = true_positives / (possible_positives + K.epsilon())
@@ -43,6 +47,7 @@ def recall(y_true, y_pred):
 
 def fbeta_score(y_true, y_pred, beta=1):
     """Computes the F score.
+
     The F score is the weighted harmonic mean of precision and recall.
     Here it is only computed as a batch-wise average, not globally.
     This is useful for multi-label classification, where input samples can be
@@ -56,6 +61,7 @@ def fbeta_score(y_true, y_pred, beta=1):
     correct classes becomes more important, and with beta > 1 the metric is
     instead weighted towards penalizing incorrect class assignments.
     """
+
     if beta < 0:
         raise ValueError('The lowest choosable beta is zero (only precision).')
 
